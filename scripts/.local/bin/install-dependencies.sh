fbd88ed1ec7137fcaac5b7e74c47801f3e855e21
Pekka <pekka@oxbacka.com>
2025-12-25 18:50:40 +0200

    start mpd

--- scripts/.local/bin/install-dependencies.sh
+++ scripts/.local/bin/install-dependencies.sh
@@ -1,5 +1,6 @@
 echo "Installing dependencies:"
-sudo pacman -Syu --needed mpd rmpc kitty hyprlock cava
+sudo pacman -Syu --needed mpd rmpc kitty hyprlock cava stow wallust nerd-fonts
 echo "Setting settings:"
 systemctl --user enable mpd
+systemctl --user start mpd
 echo "Mpd enabled"
